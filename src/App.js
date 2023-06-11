8885365870280c39a679ebe5c58d1665b3d18e39
stanley07 <ezebo001@gmail.com>
2023-06-11 16:34:18 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,212 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Web3 from 'web3';
 
 function App() {
+  const [tokens, setTokens] = useState([]);
+
+  useEffect(() => {
+    getTokensData();
+  }, []);
+
+  const getTokensData = async () => {
+    try {
+      const web3 = new Web3(window.ethereum);
+      const contractAddress = '0x2A1750561de46E284eC43C917379e516C4b64c06';
+      const abi = [
+        {
+          "inputs": [],
+          "stateMutability": "nonpayable",
+          "type": "constructor"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "tokenSymbol",
+              "type": "string"
+            }
+          ],
+          "name": "getLatestPrice",
+          "outputs": [
+            {
+              "internalType": "int256",
+              "name": "",
+              "type": "int256"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "tokenSymbol",
+              "type": "string"
+            }
+          ],
+          "name": "storeLatestPrice",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "name": "tokens",
+          "outputs": [
+            {
+              "internalType": "contract AggregatorV3Interface",
+              "name": "priceFeed",
+              "type": "address"
+            },
+            {
+              "internalType": "int256",
+              "name": "storedPrice",
+              "type": "int256"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        }
+      ];
+      const contract = new web3.eth.Contract(abi, contractAddress);
+      const tokensList = ["ETH/USD", "BTC/USD", "LINK/USD"]; // List of token symbols
+      const tokensData = await Promise.all(
+        tokensList.map(async (tokenSymbol) => {
+          const tokenData = await contract.methods.tokens(tokenSymbol).call();
+          return { symbol: tokenSymbol, price: parseInt(tokenData.storedPrice) / 100000000 };
+        })
+      );
+      setTokens(tokensData);
+      console.log("tokensData: ", tokensData);
+    } catch (error) {
+      console.log("getTokensData Error: ", error);
+    }
+  };
+
+  const updatePrice = async (tokenSymbol) => {
+    try {
+      const web3 = new Web3(window.ethereum);
+      const contractAddress = '0x2A1750561de46E284eC43C917379e516C4b64c06';
+      const abi = [
+        {
+          "inputs": [],
+          "stateMutability": "nonpayable",
+          "type": "constructor"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "tokenSymbol",
+              "type": "string"
+            }
+          ],
+          "name": "getLatestPrice",
+          "outputs": [
+            {
+              "internalType": "int256",
+              "name": "",
+              "type": "int256"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "tokenSymbol",
+              "type": "string"
+            }
+          ],
+          "name": "storeLatestPrice",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "name": "tokens",
+          "outputs": [
+            {
+              "internalType": "contract AggregatorV3Interface",
+              "name": "priceFeed",
+              "type": "address"
+            },
+            {
+              "internalType": "int256",
+              "name": "storedPrice",
+              "type": "int256"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        }
+      ];
+      const contract = new web3.eth.Contract(abi, contractAddress);
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const account = accounts[0];
+      const transaction = contract.methods.storeLatestPrice(tokenSymbol).send({ from: account });
+      await transaction;
+      await getTokensData();
+    } catch (error) {
+      console.log("updatePrice Error: ", error);
+    }
+  };
+
+  const appStyle = {
+    backgroundColor: "#189ad3", // Set the sparkling blue background color
+    padding: "20px", // Add padding to the app container
+  };
+
+  const headerStyle = {
+    textAlign: "center", // Center align the header text
+  };
+
+  const footerStyle = {
+    textAlign: "center", // Center align the footer text
+    marginTop: "auto", // Push the footer to the bottom of the page
+    padding: "10px", // Add padding to the footer
+    backgroundColor: "#f8f8f8", // Set the desired background color for the footer
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div style={appStyle}>
+      <header style={headerStyle}>
+        <h1>Crypto Monitor</h1>
       </header>
+      <div className="row mt-5">
+        {tokens.map((token, index) => (
+          <div className="col" key={index}>
+            <div className="card text-center">
+              <div className="card-header">
+                <h3>{token.symbol}</h3>
+              </div>
+              <div className="card-body">
+                <p className="card-text">Stored Price: {token.price}</p>
+                <button type="submit" className="btn btn-dark" onClick={() => updatePrice(token.symbol)}>Update</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <footer style={footerStyle}>
+        <p>Designed by Stanley Okafor for Chainlink Spring 2023 Hackathon</p>
+      </footer>
     </div>
   );
 }
